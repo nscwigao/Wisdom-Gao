6b932b8f69eac34bd5fc1507c702e70078cff0ba
Wisdom Gao <wisdom.gao@flex.com>
2017-10-23 01:09:30 -0700

    [Studio] Add screen "DataTest"

---
+++ client/controllers/app/datatest.js
@@ -0,0 +1,5 @@
+angular.module('app').controller('app_datatest', app_datatest);
+function app_datatest($scope, app) {
+    'use strict';
+    app.init($scope);
+}
--- client/states.js
+++ client/states.js
@@ -51,5 +51,14 @@ angular.module('app').config(function ($stateProvider) {
                 }
             }
         }
+    }).state('app.datatest', {
+        views: {
+            app: {
+                controller: 'app_datatest',
+                templateProvider: function (app) {
+                    return app.templateProvider('app.datatest');
+                }
+            }
+        }
     });
 });
